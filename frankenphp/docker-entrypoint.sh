054b72c2ff66d052a3cd7719ba80851d39233ef9
La-Kiks <kilian.audroin@gmail.com>
2024-07-04 18:37:12 +0200

    runtime frankenphp-symfony

--- frankenphp/docker-entrypoint.sh
+++ frankenphp/docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -e
 
+if [ "$1" = 'frankenphp' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 	if [ -z "$(ls -A 'vendor/' 2>/dev/null)" ]; then
 		composer install --prefer-dist --no-progress --no-interaction
 	fi
